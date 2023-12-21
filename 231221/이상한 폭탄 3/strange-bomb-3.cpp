db5bffadc87f522afb3e05f69ac6fc1c60cb306d
subsub-e <55149395+subsub-e@users.noreply.github.com>
2023-12-21 21:46:18 +0900

    Title: 이상한 폭탄 3, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231221/이상한 폭탄 3/strange-bomb-3.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int a, b; 
+int arr[10000001];
+
+bool InRange(int x){
+    if(x >= 0 && x < a){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+int Maxarr(int x){
+    int cnt = 0;
+    int ans = arr[x];
+    for(int i = x - 3; i < x + 3; i++){
+        if(!InRange(i)){
+            continue;
+        }
+        if(arr[i] == ans){
+            cnt++;
+        }
+    }
+    if(cnt == 1){
+        return 0;
+    }
+    else{
+        return cnt;
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> a >> b;
+    for(int i = 0; i < a; i++){
+        cin >> arr[i];
+    }
+    int max1 = 0;
+    int ans1 = 0;
+    for(int i = 0; i < a; i++){
+        int maxarr = Maxarr(i);
+        if(max1 < maxarr){
+            max1 = maxarr;
+            ans1 = max(ans1, arr[i]);
+        }
+    }
+    cout << ans1;
+    return 0;
+}
