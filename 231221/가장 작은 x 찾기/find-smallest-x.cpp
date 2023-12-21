17b7881cefd0492e78df93e56eedd140e6560a61
subsub-e <55149395+subsub-e@users.noreply.github.com>
2023-12-21 15:22:09 +0900

    Title: 가장 작은 x 찾기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 231221/가장 작은 x 찾기/find-smallest-x.cpp
+++ 231221/가장 작은 x 찾기/find-smallest-x.cpp
@@ -4,27 +4,30 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int a; cin >> a;
-    int arr[2][a];
+    int arr[a][2];
     for(int i = 0; i < a; i++){
         int b, c; cin >> b >> c;
         arr[i][0] = b;
         arr[i][1] = c;
     }
     int ans = 1;
-    int flag = 0;
-    while(ans <= 10000){
-        int d;
+    int d;
+    while(1){
         d = ans;
-        flag = 0;
+        //cout << d << '\n';
+        int flag = 0;
         for(int i = 0; i < a; i++){
-            d *= 2;
-            if(!(arr[i][0] <= d && d <= arr[i][1])){
-                ans++;
+            d = d * 2;
+            if((d >= arr[i][0]) && (d <= arr[i][1])){
                 flag = 1;
+            }
+            else{
+                flag = 0;
+                ans++;
                 break;
             }
         }
-        if(flag == 0){
+        if(flag == 1){
             break;
         }
     }
