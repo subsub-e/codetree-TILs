c4b9d17306bacd44994e9d6b21d9d25bb7f2bb60
subsub-e <55149395+subsub-e@users.noreply.github.com>
2023-12-21 14:38:38 +0900

    Title: 독서실의 거리두기 5, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231221/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
+++ 231221/독서실의 거리두기 5/study-cafe-keeping-distance-5.cpp
@@ -16,6 +16,7 @@ int main() {
     }
     int max1 = 0;
     int cnt = 0;
+    int ans;
     for(int i = 0; i < a; i++){
         if(v[i] == 1){
             cnt = 0;
@@ -23,7 +24,10 @@ int main() {
         else{
             cnt++;
             if(i == a-1){
-                max1 = max(max1, cnt);
+                if(max1 < cnt){
+                    max1 = cnt;
+                    ans = i;
+                }
                 break;
             }
             for(int j = 1; j < cnt; j++){
@@ -32,9 +36,29 @@ int main() {
                     break;
                 }
             }
-            max1 = max(max1, cnt);
+            if(max1 < cnt){
+                max1 = cnt;
+                ans = i;
+            }
         }
     }
-    cout << max1;
+    int min = 10000;
+    v[ans] = 1;
+    int cnt1 = 0;
+    for(int i = 0; i < a; i++){
+        if(v[i] == 1){
+            if(i == 0){
+                continue;
+            }
+            if(min > cnt1){
+                min = cnt1;
+            }
+            cnt1 = 0;
+        }
+        else{
+            cnt1++;
+        }
+    }
+    cout << min + 1;
     return 0;
 }
