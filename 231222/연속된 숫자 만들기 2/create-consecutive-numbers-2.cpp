91dbf739b7a962c1c1ee4c3635efe04f874154ae
subsub-e <55149395+subsub-e@users.noreply.github.com>
2023-12-22 20:11:33 +0900

    Title: 연속된 숫자 만들기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231222/연속된 숫자 만들기 2/create-consecutive-numbers-2.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b, c;
+    cin >> a >> b >> c;
+    int arr[3];
+    arr[0] = a;
+    arr[1] = b;
+    arr[2] = c;
+
+    sort(arr, arr + 2);
+    int cnt = 0;
+    while(1){
+        if((arr[1] - arr[0] == 1) && (arr[2] - arr[1] == 1)){
+            break;
+        }
+        if((arr[1] - arr[0] == 1) || (arr[2] - arr[1] == 1)){
+            cnt++;
+            break;
+        }
+        else{
+            if((arr[1] - arr[0]) > (arr[2] - arr[1])){
+                arr[0] = (arr[1] + arr[2]) / 2;
+                cnt++;
+                sort(arr, arr + 2);
+            }
+            else{
+                arr[2] = (arr[0] + arr[1]) / 2;
+                cnt++;
+                sort(arr, arr + 2);
+            }
+        }
+    }
+    cout << cnt;
+    return 0;
+}
