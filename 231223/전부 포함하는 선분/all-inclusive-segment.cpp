dd5b71cb444f78313e52af3171312eaff05cab87
subsub-e <55149395+subsub-e@users.noreply.github.com>
2023-12-23 18:17:05 +0900

    Title: 전부 포함하는 선분, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 231223/전부 포함하는 선분/all-inclusive-segment.cpp
+++ 231223/전부 포함하는 선분/all-inclusive-segment.cpp
@@ -17,6 +17,10 @@ int main() {
             v2.push_back(b);
         }
     }
+    if(a == 2){
+        cout << min((v1[1] - v1[0]),(v2[1] - v2[0]));
+        return 0;
+    }
     sort(v1.begin(), v1.end());
     sort(v2.begin(), v2.end());
     int length1 = v2[a-1] - v1[1];
