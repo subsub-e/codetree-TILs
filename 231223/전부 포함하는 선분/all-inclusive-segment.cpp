6b8a473ec7126eeef5f9917f291a48344e6690b8
subsub-e <55149395+subsub-e@users.noreply.github.com>
2023-12-23 18:14:20 +0900

    Title: 전부 포함하는 선분, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231223/전부 포함하는 선분/all-inclusive-segment.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a; cin >> a;
+    vector<int> v1;
+    vector<int> v2;
+    for(int i= 0; i < a * 2; i++){
+        int b; cin >> b;
+        if(i % 2 == 0){
+            v1.push_back(b);
+        }
+        else{
+            v2.push_back(b);
+        }
+    }
+    sort(v1.begin(), v1.end());
+    sort(v2.begin(), v2.end());
+    int length1 = v2[a-1] - v1[1];
+    int length2 = v2[a-2] - v1[0];
+    cout << min(length1, length2);
+    return 0;
+}
