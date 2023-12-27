537f15d6fbf6f932bd8fe79bc031758ada938d20
subsub-e <55149395+subsub-e@users.noreply.github.com>
2023-12-27 15:22:03 +0900

    Title: 거품 정렬 구현, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231227/거품 정렬 구현/implement-bubble-sort.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a; cin >> a;
+    int arr[a];
+    for(int i = 0; i < a; i++){
+        cin >> arr[i];
+    }
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < a; j++){
+            int temp;
+            if(arr[j] > arr[j + 1]){
+                temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    for(int i = 0; i < a; i++){
+        cout << arr[i] << ' ';
+    }
+    return 0;
+}
