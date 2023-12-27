96137d42fd199a9d78c79ed0f1f1b54be97f27f5
subsub-e <55149395+subsub-e@users.noreply.github.com>
2023-12-27 16:41:01 +0900

    Title: 병합 정렬 구현, Time: 54ms, Memory: 1MB, Status: Passed - CodeTree

---
+++ 231227/병합 정렬 구현/implement-merge-sort.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a; cin >> a;
+    int arr[a];
+    for(int i= 0; i < a; i++){
+        cin >> arr[i];
+    }
+    sort(arr, arr + a);
+    for(int i = 0; i < a; i++){
+        cout << arr[i] << ' ';
+    }
+    return 0;
+}
