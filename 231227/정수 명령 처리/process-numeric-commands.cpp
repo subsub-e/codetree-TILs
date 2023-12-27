646537a2cf2b0955cddfcd03970d749795e952e2
subsub-e <55149395+subsub-e@users.noreply.github.com>
2023-12-27 22:34:41 +0900

    Title: 정수 명령 처리, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 231227/정수 명령 처리/process-numeric-commands.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a; cin >> a;
+    stack<int> s;
+    for(int i = 0; i < a; i++){
+        string str; cin >> str;
+        int b;
+        if(str == "push"){
+            cin >> b;
+            s.push(b);
+        }
+        else if(str == "size"){
+            cout << s.size() << '\n';
+        }
+        else if(str == "empty"){
+            if(s.size() == 0){
+                cout << 1 << '\n';
+            }
+            else{
+                cout << 0 << '\n';
+            }
+        }
+        else if(str == "pop"){
+            cout << s.top() << '\n';
+            s.pop();
+        }
+    }
+    return 0;
+}
