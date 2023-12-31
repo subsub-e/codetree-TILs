a13d388e79b471da7fa57d299639c66d393a9c8b
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-01-01 00:14:04 +0900

    Title: 기울어진 직사각형, Time: 11ms, Memory: 0MB, Status: Passed - CodeTree

--- 240101/기울어진 직사각형/slanted-rectangle.cpp
+++ 240101/기울어진 직사각형/slanted-rectangle.cpp
@@ -1,89 +1,61 @@
 #include <iostream>
 #include <algorithm>
+
+#define MAX_N 20
+#define DIR_NUM 4
+
 using namespace std;
 
-int a;
-int arr[20][20];
+int n;
+int grid[MAX_N][MAX_N];
 
 bool InRange(int x, int y) {
-    return x >= 0 && x < a && y >= 0 && y < a;
+    return 0 <= x && x < n && 0 <= y && y < n;
 }
 
-int GetNum(int x, int y){
-    int num = 0;
-    int ox = x;
-    int oy = y;
-    int dx[4] = {-1, -1, 1, 1};
-    int dy[4] = {1, -1, -1, 1};
+int GetScore(int x, int y, int k, int l) {
+    int dx[DIR_NUM] = {-1, -1, 1, 1};
+    int dy[DIR_NUM] = {1, -1, -1, 1};
+    int move_num[DIR_NUM] = {k, l, k, l};
+    
+    int sum_of_nums = 0;
 
-    num += arr[x][y];
-    for(int i = 0; i < 4; i++){
-        if(i == 0){
-            while(1){
-                x += dx[i];
-                y += dy[i];
-                if(x < 1 || y > a - 1){
-                    x -= dx[i];
-                    y -= dy[i];
-                    break;
-                }
-                num += arr[x][y];
-            }
+    // 기울어진 직사각형의 경계를 쭉 따라가봅니다.
+    for(int d = 0; d < DIR_NUM; d++)
+        for(int q = 0; q < move_num[d]; q++) {
+            x += dx[d]; y += dy[d];
+                
+            // 기울어진 직사각형이 경계를 벗어나는 경우라면
+            // 불가능하다는 의미로 답이 갱신되지 않도록
+            // 0을 반환합니다.
+            if(!InRange(x, y))
+                return 0;
+			
+            sum_of_nums += grid[x][y];
         }
-        else if(i == 1){
-            while(1){
-                x += dx[i];
-                y += dy[i];
-                if(x < 0 || y == 0){
-                    x -= dx[i];
-                    y -= dy[i];
-                    break;
-                }
-                num += arr[x][y];
-            }
-        }
-        else if(i == 2){
-            while(1){
-                x += dx[i];
-                y += dy[i];
-                if(x == a-1 || y < 0){
-                    x -= dx[i];
-                    y -= dy[i];
-                    break;
-                }
-                num += arr[x][y];
-            }
-        }
-        else if(i == 3){
-            while(1){
-                x += dx[i];
-                y += dy[i];
-                if(x == ox || y == oy){
-                    break;
-                }
-                num += arr[x][y];
-            }
-        }
-    }
-    return num;
+    
+    return sum_of_nums;
 }
 
 int main() {
-    // 여기에 코드를 작성해주세요.
-    cin >> a;
-    for(int i = 0; i < a; i++){
-        for(int j = 0; j < a; j++){
-            cin >> arr[i][j];
-        }
-    }
-    int maxans = 0;
-    for(int i = 2; i < a; i++){
-        for(int j = 1; j < a - 1; j++){
-            int num_of_Tot = GetNum(i, j);
-            maxans = max(maxans, num_of_Tot);
-            //cout << i << ' ' << j << ' ' << num_of_Tot << '\n';
-        }
-    }
-    cout << maxans;
+    cin >> n;
+    
+    for(int i = 0; i < n; i++)
+        for(int j = 0; j < n; j++)
+            cin >> grid[i][j];
+    
+    int ans = 0;
+    
+    // (i, j)를 시작으로 1, 2, 3, 4 방향
+    // 순서대로 길이 [k, l, k, l] 만큼 이동하면 그려지는
+    // 기울어진 직사각형을 잡아보는
+    // 완전탐색을 진행해봅니다.
+    for(int i = 0; i < n; i++)
+		for(int j = 0; j < n; j++)
+			for(int k = 1; k < n; k++)
+				for(int l = 1; l < n; l++)
+                    ans = max(ans, GetScore(i, j, k, l));
+
+    cout << ans;
     return 0;
 }
