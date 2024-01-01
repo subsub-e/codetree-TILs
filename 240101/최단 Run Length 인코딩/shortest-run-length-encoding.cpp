fa123d7c77e5aaaca67b90795ae70ea9d6a8671d
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-01-01 14:43:53 +0900

    Title: 최단 Run Length 인코딩, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240101/최단 Run Length 인코딩/shortest-run-length-encoding.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+string str; 
+
+int Strlen(string str){
+    int cnt = 1;
+    for(int i = 1; i < str.length(); i++){
+        if(str[i] != str[i - 1]){
+            cnt++;
+        }
+    }
+    return cnt * 2;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> str;
+    int minans = 11;
+    int flag = 1;
+    if(str.length() == 10){
+        for(int i = 1; i < str.length(); i++){
+            if(str[i] != str[i-1]){
+                flag = 0;
+            }
+        }
+    }
+    if(flag == 1){
+        cout << 3;
+        return 0;
+    }
+    for(int i = 0; i < str.length(); i++){
+        char temp = str[str.length() - 1];
+        for(int j = str.length() - 1; j > 0; j--){
+            str[j] = str[j-1];
+        }
+        str[0] = temp;
+        minans = min(minans, Strlen(str));
+    }
+    cout << minans;
+    return 0;
+}
