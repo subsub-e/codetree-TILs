7cf72d9ba0d530792d1f5aec5249b0c0b879bc4f
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-01-01 17:20:38 +0900

    Title: 떨어지는 1자 블록, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240101/떨어지는 1자 블록/falling-horizontal-block.cpp
+++ 240101/떨어지는 1자 블록/falling-horizontal-block.cpp
@@ -1,29 +1,35 @@
 #include <iostream>
 using namespace std;
 
+int a, b, c; 
+int arr[100][100];
+
+
+int find(){
+    int index = 0;
+    for(int i = 0; i < a; i++){
+        for(int j = c; j < b; j++){
+            if(arr[i][j] == 1){
+                return index;
+            }
+        }
+        index = i;
+    }
+    return index;
+}
+
 int main() {
     // 여기에 코드를 작성해주세요.
-    int a, b, c; cin >> a >> b >> c;
-    int arr[a][a];
+    cin >> a >> b >> c;
     for(int i = 0; i < a; i++){
         for(int j = 0; j < a; j++){
             cin >> arr[i][j];
         }
     }
     c--;
-    for(int i = a - 1; i >= 0; i--){
-        int flag = 0;
-        for(int j = c; j < b; j++){
-            if(arr[i][j] == 1){
-                flag = 1;
-            }
-        }
-        if(flag == 0){
-            for(int j = c; j < b; j++){
-                arr[i][j] = 1;
-            }
-            break;
-        }
+    int index_i = find();
+    for(int i = 0; i < b; i++){
+        arr[index_i][i] = 1;
     }
     for(int i = 0; i < a; i++){
         for(int j = 0; j < a; j++){
