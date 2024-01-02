0fe6bebae1840f72364c1e4e29b7f1cd025cc44c
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-01-03 02:19:27 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240103/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
+using namespace std;
+
+int a;
+vector<int> v;
+unordered_set<string> visited;
+
+bool possible() {
+    for (int i = 1; i < a; i++) {
+        if (v[i - 1] == v[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+string getSequenceString() {
+    string sequence;
+    for (int i = 0; i < a; i++) {
+        sequence += to_string(v[i]);
+    }
+    return sequence;
+}
+
+void simulate(int len, int start) {
+    if (len == a) {
+        if (possible()) {
+            string sequence = getSequenceString();
+            if (visited.find(sequence) == visited.end()) {
+                visited.insert(sequence);
+                for (int i = 0; i < a; i++) {
+                    cout << v[i];
+                }
+                cout << '\n';
+                exit(0);  // 프로그램 종료 추가
+            }
+        }
+        return;
+    }
+
+    for (int i = start; i <= 6; i++) {
+        v.push_back(i);
+        simulate(len + 1, i + 1);
+        v.pop_back();
+    }
+}
+
+int main() {
+    cin >> a;
+    simulate(0, 4);
+    return 0;
+}
