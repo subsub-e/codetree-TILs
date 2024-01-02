9599e49b9138ffcd95566cc27f1b5ae6f9253d31
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-01-03 02:23:15 +0900

    Title: 가능한 수열 중 최솟값 구하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240103/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.cpp
+++ 240103/가능한 수열 중 최솟값 구하기/find-min-of-possible-series.cpp
@@ -1,52 +1,71 @@
 #include <iostream>
-#include <vector>
-#include <unordered_set>
 #include <algorithm>
+#include <vector>
+#include <cstdlib>
+
+#define NUM_LEN 3
+
 using namespace std;
 
-int a;
-vector<int> v;
-unordered_set<string> visited;
+int n;
+vector<int> series;
+int numbers[NUM_LEN] = {4, 5, 6};
 
-bool possible() {
-    for (int i = 1; i < a; i++) {
-        if (v[i - 1] == v[i]) {
+// 시작과 끝 인덱스가 주어진 두 수열이 동일한지 여부를 판별합니다.
+bool IsEqual(int start1, int end1, int start2, int end2) {
+    for(int i = 0; i <= end1 - start1; i++) {
+        if(series[start1 + i] != series[start2 + i])
             return false;
-        }
     }
+
     return true;
 }
 
-string getSequenceString() {
-    string sequence;
-    for (int i = 0; i < a; i++) {
-        sequence += to_string(v[i]);
+// 가능한 수열인지 여부를 판별합니다.
+bool IsPossibleSeries() {
+    int len = 1;
+    while(true) {
+        // 연속 부분 수열의 시작과 끝 인덱스를 설정하여 줍니다.
+        int end1 = (int) series.size() - 1, start1 = end1 - len + 1;
+        int end2 = start1 - 1, start2 = end2 - len + 1;
+
+        if(start2 < 0)
+            break;
+
+        // 인접한 연속 부분 수열이 같은지 여부를 확인합니다.
+        if(IsEqual(start1, end1, start2, end2))
+            return false;
+
+        len++;
     }
-    return sequence;
+
+    return true;
 }
 
-void simulate(int len, int start) {
-    if (len == a) {
-        if (possible()) {
-            for (int i = 0; i < a; i++) {
-                cout << v[i];
-            }
-            cout << '\n';
-            exit(0);  // 프로그램 종료 추가
-            
-        }
-        return;
-    }   
+void FindMinSeries(int cnt) {
+    // n개의 숫자가 선택됐을 때 불가능한 수열인 경우 탐색을 종료합니다.
+    // 가능한 수열인 경우 이를 출력하고 프로그램을 종료합니다.
+    if(cnt == n) {
+        for(int i = 0; i < series.size(); i++)
+            cout << series[i];
+        exit(0);
+    }
 
-    for (int i = start; i <= 6; i++) {
-        v.push_back(i);
-        simulate(len + 1, i + 1);
-        v.pop_back();
+    // 사용 가능한 각 숫자가 뽑혔을 때의 경우를 탐색합니다.
+    for(int i = 0; i < NUM_LEN; i++) {
+        series.push_back(numbers[i]);
+        // 해당 시점까지 만들 수열이 조건을 만족하는 경우에만
+        // 탐색을 진행합니다.
+        if(IsPossibleSeries())
+            FindMinSeries(cnt + 1);
+        
+        series.pop_back();
     }
 }
 
 int main() {
-    cin >> a;
-    simulate(0, 4);
+    cin >> n;
+
+    FindMinSeries(0);
     return 0;
 }
