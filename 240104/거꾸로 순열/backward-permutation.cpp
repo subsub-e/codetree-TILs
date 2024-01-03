753f6ecf49b59646250c01559e2e718c4a11da1e
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-01-04 02:29:56 +0900

    Title: 거꾸로 순열, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240104/거꾸로 순열/backward-permutation.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int a;
+bool visited[8];
+vector<int> v;
+
+void simulate(int count){
+    if(count == a){
+        for(int i = 0; i < a; i++){
+            cout << v[i] << ' ';
+        }
+        cout << '\n';
+    }
+
+    for(int i = a; i > 0; i--){
+        if(visited[i] == true){
+            continue;
+        }
+
+        visited[i] = true;
+        v.push_back(i);
+        simulate(count + 1);
+        v.pop_back();
+        visited[i] = false;
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> a;
+    simulate(0);
+    return 0;
+}
