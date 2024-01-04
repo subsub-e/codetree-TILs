e4dc0ddf0958717ff1e83fb57ae7de6c8fa68e96
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-01-05 01:28:18 +0900

    Title: 두 방향 탈출 가능 여부 판별하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240105/두 방향 탈출 가능 여부 판별하기/determine-escapableness-with-2-ways.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+using namespace std;
+
+int n, m;
+int arr[101][101];
+bool visited[101][101];
+
+bool InRange(int x, int y){
+    return 0 <= x && x < n && 0 <= y && y < m;
+}
+
+bool cango(int x, int y){
+    if(!InRange(x, y)){
+        return false;
+    }
+    if(visited[x][y] || arr[x][y] == 0){
+        return false;
+    }
+    return true;
+}
+
+bool dfs(int x, int y){
+    if(x == n-1 && y == m-1){
+        return true;
+    }
+
+    int dx[2] = {1, 0};
+    int dy[2] = {0, 1};
+
+    for(int i = 0; i < 2; i++){
+        int new_x = x + dx[i];
+        int new_y = y + dy[i];
+        if(cango(new_x, new_y)){
+            visited[new_x, new_y];
+            dfs(new_x, new_y);
+        }
+    }
+    return false;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cin >> arr[i][j];
+        }
+    }
+    if(dfs(0,0)){
+        cout << 1;
+        return 0;
+    }
+    cout << 0;
+    return 0;
+}
