66d5a3d804d5cde456210cbc59256508cc2fbfad
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-01-05 02:28:33 +0900

    Title: 마을 구분하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240105/마을 구분하기/seperate-village.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n;
+int arr[26][26];
+int ans;
+vector<int> v;
+bool visited[26][26];
+
+bool InRange(int x, int y){
+    return 0 <= x && x < n && 0 <= y && y < n;
+}
+
+bool cango(int x, int y){
+    if(!InRange(x, y)){
+        return false;
+    }
+    if(arr[x][y] == 0 || visited[x][y]){
+        return false;
+    }
+    return true;
+}
+
+int dfs(int x, int y){
+    int dx[4] = {-1, 1, 0, 0};
+    int dy[4] = {0, 0, -1, 1};
+
+    for(int i = 0; i < 4; i++){
+        int new_x = x + dx[i];
+        int new_y = y + dy[i];
+
+        if(cango(new_x, new_y)){
+            visited[new_x][new_y] = 1;
+            ans++;
+            //cout << new_x << ' ' << new_y << ' ' << ans << '\n';
+            dfs(new_x, new_y);
+        }
+    }
+    return ans;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> arr[i][j];
+        }
+    }
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            if(!visited[i][j] && arr[i][j] == 1){
+                dfs(i, j);
+                v.push_back(ans);
+                ans = 0;
+            }
+        }
+    }
+    //dfs(0,0);
+    cout << v.size() << '\n'; 
+    sort(v.begin(), v.end());
+    for(int i = 0; i < v.size(); i++){
+        cout << v[i] << '\n';
+    }
+    return 0;
+}
