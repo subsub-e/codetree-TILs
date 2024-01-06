6e09a7a1a71046674d9e45007249b71b946d95c5
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-01-06 23:39:38 +0900

    Title: 최소 경로로 탈출 하기, Time: 8ms, Memory: 0MB, Status: Passed - CodeTree

--- 240106/최소 경로로 탈출 하기/escape-with-min-distance.cpp
+++ 240106/최소 경로로 탈출 하기/escape-with-min-distance.cpp
@@ -1,26 +1,39 @@
 #include <iostream>
+#include <queue>
 using namespace std;
 
 int n, m;
 int arr[100][100];
 int step[100][100];
 bool visited[100][100];
+queue<pair<int, int>> q;
 
 bool cango(int x, int y){
     return 0 <= x && x < n && 0 <= y && y < m && !visited[x][y] && arr[x][y];
 }
 
-void bps(int x, int y){
+void push(int x, int y, int s){
+    step[x][y] = s;
+    visited[x][y] = 1;
+    q.push(make_pair(x, y));
+}
+
+void bps(){
     int dx[4] = {1, 0, -1, 0};
     int dy[4] = {0, 1, 0, -1};
 
-    for(int i = 0; i < 4; i++){
-        int pos_x = x + dx[i];
-        int pos_y = y + dy[i];
-        if(cango(pos_x, pos_y)){
-            visited[pos_x][pos_y] = 1;
-            step[pos_x][pos_y] = step[x][y] + 1;
-            bps(pos_x, pos_y);
+    while(!q.empty()) {
+        pair<int, int> curr_pos = q.front();
+        q.pop();
+
+        int x = curr_pos.first;
+        int y = curr_pos.second;
+        for(int i = 0; i < 4; i++){
+            int pos_x = x + dx[i];
+            int pos_y = y + dy[i];
+            if(cango(pos_x, pos_y)){
+                push(pos_x, pos_y, step[x][y] + 1);
+            }
         }
     }
 }
@@ -33,10 +46,19 @@ int main() {
             cin >> arr[i][j];
         }
     }
-    visited[0][0] = 1;
-    step[0][0] = 0;
-    bps(0, 0);
+    push(0, 0, 0);
+    bps();
     //for(int i = 0; i < n; i++)
+    if(step[n-1][m-1] == 0){
+        cout << -1;
+        return 0;
+    }
+    // for(int i = 0; i < n; i++){
+    //     for(int j = 0; j < m; j++){
+    //         cout << step[i][j] << ' ';
+    //     }
+    //     cout << '\n';
+    // }
     cout << step[n-1][m-1];
     return 0;
 }
