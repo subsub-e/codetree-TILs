da41fea24969e70fcf9c85576e655467c1c14fab
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-01-09 21:33:24 +0900

    Title: 정수 사각형 최솟값의 최대, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240109/정수 사각형 최솟값의 최대/maximin-path-in-square.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int a;
+int arr[100][100];
+int answer[100][100];
+
+void init(){
+    answer[0][0] = arr[0][0];
+    for(int i = 1; i < a; i++){
+        answer[i][0] = min(arr[i][0], arr[i-1][0]);
+        answer[0][i] = min(arr[0][i], arr[0][i-1]);
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> a;
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < a; j++){
+            cin >> arr[i][j];
+        }
+    }
+    init();
+    
+    for(int i = 1; i < a; i++){
+        for(int j = 1; j < a; j++){
+            int m = max(answer[i - 1][j], answer[i][j - 1]);
+            answer[i][j] = min(m, arr[i][j]);
+        }
+    }
+    // for(int i = 0; i < a; i++){
+    //     for(int j = 0; j < a; j++){
+    //         cout << answer[i][j] << ' ';
+    //     }
+    //     cout << '\n';
+    // }
+    // int ans_m = max(arr[a-2][a-1], arr[a-1][a-2]);
+    // if(ans_m <= arr[a-1][a-1]){
+    //     answer[a-1][a-1] = ans_m;
+    // }
+    // else{
+    //     answer[a-1][a-1] = arr[a-1][a-1];
+    // }
+    cout <<answer[a-1][a-1];
+    return 0;
+}
