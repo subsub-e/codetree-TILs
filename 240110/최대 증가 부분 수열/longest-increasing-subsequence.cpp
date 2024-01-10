3f1ada5dfff1ac51b01bf2f5cbf84bd1244186e0
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-01-10 16:03:26 +0900

    Title: 최대 증가 부분 수열, Time: 7ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240110/최대 증가 부분 수열/longest-increasing-subsequence.cpp
+++ 240110/최대 증가 부분 수열/longest-increasing-subsequence.cpp
@@ -5,8 +5,8 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int a; cin >> a;
-    int arr[a];
-    int answer[a] = {};
+    int arr[1001];
+    int answer[1001] = {};
     for(int i = 0; i < a; i++){
         cin >> arr[i];
     }
