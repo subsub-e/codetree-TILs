dc9374256b483981c81a09b14ddb65bdc02a8048
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-01-10 18:26:16 +0900

    Title: 겹치지 않게 선분 고르기 2, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 240110/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.cpp
+++ 240110/겹치지 않게 선분 고르기 2/select-segments-without-overlap-2.cpp
@@ -1,30 +1,25 @@
-#include <iostream>
 #include <algorithm>
+#include <iostream>
+#include <vector>
 using namespace std;
-
-int a;
-int answer[1001];
-pair<int, int> arr[1001];
-
+int n, ans;
+int dp_table[1005];
+pair<int, int> line_pos[1005];
 int main() {
-    // 여기에 코드를 작성해주세요.
-    cin >> a;
-    for(int i = 1; i <= a; i++){
-        cin >> arr[i].first >> arr[i].second;
-    }
-    answer[arr[1].second] = 1;
-    for(int i = 2; i <= a; i++){
-        int start_x = arr[i].first;
-        int start_y = arr[i].second;
-        for(int j = 1; j < start_x; j++){
-            answer[start_y] = max(answer[start_y], answer[j]);
-        }
-        answer[start_y]++;
-    }
-    int ans = 0;
-    for(int i = 1; i <= 1000; i++){
-        ans = max(ans, answer[i]);
-    }
-    cout << ans;
-    return 0;
-}
+  cin >> n;
+  for (int i = 1; i <= n; i++)
+    cin >> line_pos[i].first >> line_pos[i].second;
+
+  sort(line_pos + 1, line_pos + n + 1);
+  dp_table[line_pos[1].second] = 1;
+  for (int i = 2; i <= n; i++) {
+    int start_pos = line_pos[i].first, end_pos = line_pos[i].second;
+    for (int j = 1; j < start_pos; j++)
+      dp_table[end_pos] = max(dp_table[end_pos], dp_table[j] + 1);
+    dp_table[end_pos];
+  }
+  for (int i = 1; i <= 1000; i++)
+    ans = max(ans, dp_table[i]);
+  cout << ans;
+  return 0;
+};
