f9e0744b4a4c3fffc32377bf54727f6b9e6352a1
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-01-14 16:00:40 +0900

    Title: 동전 거슬러주기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240114/동전 거슬러주기/coin-change.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int n, m;
+vector<int> v;
+vector<int> wa;
+int arr[10001];
+
+void init(){
+    for(int i = 0; i < m; i++){
+        arr[i] = INT_MIN;
+    }
+    arr[0] = 0;
+}
+
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        int coin;
+        cin >> coin;
+        v.push_back(coin);
+    }
+    init();
+    for(int i = 1; i <= m; i++){
+        for(int j = 0; j < n; j++){
+            if(i >= v[j]){
+                if(arr[i - v[j]] == INT_MIN){
+                    continue;
+                }
+                wa.push_back(arr[i - v[j]] + 1);
+            }
+        }
+        if(wa.size() > 0){
+            sort(wa.begin(), wa.end());
+            // for(int k = 0; k < wa.size(); k++){
+            //     cout << wa[k] << ' ';
+            // }
+            //cout << '\n';
+            arr[i] = wa[0];
+            wa.clear();
+        }
+    }
+    if(arr[m] == INT_MIN){
+        cout << -1;
+        return 0;
+    }
+    // for(int i = 0; i <= m; i++){
+    //     cout << arr[i] << ' ';
+    // }
+    // cout << '\n';
+    cout << arr[m];
+    return 0;
+}
