b757718fad5997ee974619b205b25a2d72aff9ab
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-01-16 00:21:56 +0900

    Title: 원소의 합이 0, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240116/원소의 합이 0/the-sum-of-the-elements-is-0.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <unordered_map>
+#include <vector>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a; cin >> a;
+    unordered_map<int, int> first;
+    unordered_map<int, int> second;
+    int arr[4][a];
+    for(int i = 0; i < 4; i++){
+        for(int j = 0; j < a; j++){
+            cin >> arr[i][j];
+        }
+    }
+    for(int i = 0; i < a; i++){
+        for(int j = 0; j < a; j++){
+            first[arr[0][i] + arr[1][j]]++;
+            second[arr[2][i] + arr[3][j]]++;
+        }
+    }
+    int cnt = 0;
+    for(unordered_map<int, int>::iterator it=first.begin(); it != first.end(); it++){
+        int diff = -(it->first); // 0-(A+B)
+        if(second.find(diff) != second.end()) // 만약 그 차이가 C+D에 있다면
+            cnt++; // (해당 A+B 값의 freq) * (C+D 값의 freq) 를 ans에 더하기
+    }
+    // 출력
+    cout << cnt;
+    return 0;
+}
