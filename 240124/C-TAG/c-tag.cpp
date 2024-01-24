6ea8d2b115a65129af52dcc900734573e6b3b354
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-01-24 23:58:24 +0900

    Title: C-TAG, Time: 1179ms, Memory: 0MB, Status: Passed - CodeTree

--- 240124/C-TAG/c-tag.cpp
+++ 240124/C-TAG/c-tag.cpp
@@ -1,70 +1,55 @@
-// 50C3 -> 약 2만
-// 2만 * 1000 -> 2천만 
-
 #include <iostream>
 #include <unordered_set>
-#include <string>
-
 using namespace std;
 
-int n, m, offset[3], cnt;
-string a[1005], b[1005];
+#define MAX_N 500
 
-void backtracking(int curr, int last) {
-    if(curr == 3) {
+// 변수 선언
+int n, m;
+int ans;
+string A[MAX_N], B[MAX_N];
+unordered_set<string> s;
 
-        // for(int i = 0; i < 3; ++i) cout << offset[i] << ' ';
-        // cout << endl;
+bool TestLocation(int x, int y, int z) {
+    // x, y, z번째 자릿수를 선택했을 때 A와 B 그룹이
+    // 완벽하게 구분되면 true, 그렇지 않다면 false를 반환합니다.
+    s.clear();
 
-        unordered_set<string> all;
-
-        unordered_set<string> arr;
-        for(int j = 0; j < n; ++j) {
-            string temp = "";
-            for(int i = 0; i < 3; ++i) {
-                temp += a[j][offset[i]];
-            }
-            arr.insert(temp);
-            all.insert(temp);
-        }
-
-
-        unordered_set<string> arr2;
-        for(int j = 0; j < n; ++j) {
-            string temp = "";
-            for(int i = 0; i < 3; ++i) {
-                temp += b[j][offset[i]];
-            }
-            arr2.insert(temp);
-            all.insert(temp);
-        }
-        
-        // 두 개로 구분 된다 -> 두 개의 집합의 공집합이 0 이다.
-        if(arr.size() + arr2.size() == all.size()) ++cnt;
-        
-        return;
+    // A의 원소를 전부 HashSet에 넣어줍니다.
+    for(int i = 0; i < n; i++) {
+        s.insert(A[i].substr(x, 1) + A[i].substr(y, 1) + A[i].substr(z, 1));
     }
 
-    // 선택 o
-    for(int i = last + 1; i < m; ++i) {
-        offset[curr] = i;
-        backtracking(curr + 1, i);   
+    // B의 원소 중 하나라도 A와 같은 경우가 있다면
+    // A와 B를 구분해낼 수 없습니다.
+    for(int i = 0; i < n; i++) {
+        if(s.find(B[i].substr(x, 1) + B[i].substr(y, 1) + B[i].substr(z, 1)) != s.end())
+            return false;
     }
 
+    // 모든 B의 원소가 A와 다르다면 A와 B를 구분해낼 수 있습니다.
+    return true;
 }
 
 int main() {
-    ios::sync_with_stdio(0), cin.tie(0);
+    // 입력:
     cin >> n >> m;
-    // input
-    for(int i = 0; i < n; ++i) cin >> a[i];
-    for(int i = 0; i < n; ++i) cin >> b[i];
+
+    for(int i = 0; i < n; i++)
+        cin >> A[i];
     
-    // backtracking
-    backtracking(0, -1);
+    for(int i = 0; i < n; i++)
+        cin >> B[i];
 
-    // output
-    cout << cnt;
-
+    // 서로 다른 세 자리의 조합을 모두 순회합니다.
+    for (int i = 0; i < m; i++)
+        for (int j = i + 1; j < m; j++)
+            for (int k = j + 1; k < m; k++)
+                // i, j, k 번째 자리를 선택했을 때 두 그룹을
+                // 완벽하게 구분할 수 있는지 확인합니다.
+                if (TestLocation(i, j, k)) ans++;
+    
+    // 두 그룹을 구분해낼 수 있는 조합 수를 출력합니다.
+    cout << ans;
     return 0;
 }
