d76439f2bdb5e6ae1c9e49d128ba2d2b3678e5fc
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-01-24 19:26:56 +0900

    Title: 자리 바꾸기 2, Time: 512ms, Memory: 35MB, Status: Passed - CodeTree

--- 240124/자리 바꾸기 2/changing-seats-2.cpp
+++ 240124/자리 바꾸기 2/changing-seats-2.cpp
@@ -1,39 +1,53 @@
 #include <iostream>
 #include <unordered_set>
-#include <vector>
 using namespace std;
 
-int main() {
-    // 여기에 코드를 작성해주세요.
-    int n, k; cin >> n >> k;
-    unordered_set<int> s[n];
-    vector<pair<int, int>> v;
-    int arr[n];
-    for(int i = 0; i < k; i++){
-        int a, b; cin >> a >> b;
-        a--;
-        b--;
-        v.push_back({a, b});
-    }
-    for(int i = 0; i < n; i++){
+#define MAX_N 100000
+#define MAX_K 100000
+
+// 변수 선언
+int n, k;
+int a[MAX_K], b[MAX_K];
+int arr[MAX_N + 1];
+int ans[MAX_N + 1];
+unordered_set<int> s[MAX_N + 1];
+
+int main(){
+    // 입력:
+    cin >> n >> k;
+    for(int i = 0; i < k; i++)
+        cin >> a[i] >> b[i];
+    
+    // 시작 array를 arr에 표시합니다.
+    for(int i = 1; i <= n; i++) {
         arr[i] = i;
+        // 각 i 숫자가 위치할 수 있는 자리를 HashSet에 넣어 저장합니다.
         s[i].insert(i);
+        ans[i] = 1;
     }
-    for(int i = 0; i <= 3; i++){
-        for(int j = 0; j < k; j++){
-            int temp = arr[v[j].first];
-            arr[v[j].first] = arr[v[j].second];
-            arr[v[j].second] = temp;
-            s[arr[v[j].first]].insert(v[j].second);
-            s[arr[v[j].second]].insert(v[j].first);
-        }
-        // for(int j = 0; j < n; j++){
+
+    // 3k번 자리를 바꿉니다.
+    for(int cnt = 0; cnt < 3; cnt++) {
+        for(int i = 0; i < k; i++) {
+            // a[i]와 b[i]의 자리를 바꿔줍니다.
+            // 이때 자리가 변화한 값은 arr[a[i]]와 arr[b[i]]밖에 없습니다.
+            // 이 값들만 새로운 자리를 HashSet에 넣어 저장합니다.
+            swap(arr[a[i]], arr[b[i]]);
+
+            if(s[arr[a[i]]].find(a[i]) == s[arr[a[i]]].end()) {
+                s[arr[a[i]]].insert(a[i]);
+                ans[arr[a[i]]]++;
+            }
             
-        // }
-        //cout << '\n';
+            if(s[arr[b[i]]].find(b[i]) == s[arr[b[i]]].end()) {
+                s[arr[b[i]]].insert(b[i]);
+                ans[arr[b[i]]]++;
+            }
+        }
     }
-    for(int i = 0; i < n; i++){
-        cout << s[i].size() << '\n';
-    }
+    
+    // 3k번 자리를 바꾸면서 각 숫자들이 위치할 수 있는 값들의 개수를 ans 배열에서 확인합니다.
+    for(int i = 1; i <= n; i++)
+        cout << ans[i] << endl;
     return 0;
 }
