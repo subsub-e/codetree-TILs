a8f0f0ad0c0989a2820105dcba4e5d91d79d02f1
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-01-26 22:02:12 +0900

    Title: 마지막으로 남은 숫자, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240126/마지막으로 남은 숫자/last-remaining-number.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a; cin >> a;
+    priority_queue<int> pq;
+    for(int i = 0; i < a; i++){
+        int b; cin >> b;
+        pq.push(b);
+    }
+    while(1){
+        if(pq.size() < 2){
+            break;
+        }
+        int first = pq.top();
+        pq.pop();
+        int second = pq.top();
+        pq.pop();
+        pq.push(first - second);
+    }
+    if(pq.empty()){
+        cout << -1;
+        return 0;
+    }
+    cout << pq.top();
+    return 0;
+}
