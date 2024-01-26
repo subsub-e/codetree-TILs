714fa74c79e7003e03d36b803ea15682cdba5bc4
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-01-26 18:42:03 +0900

    Title: top k 숫자, Time: 72ms, Memory: 2MB, Status: Passed - CodeTree

---
+++ 240126/top k 숫자/top-k-elements.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a, b; cin >> a >> b;
+    set<int> s;
+    int cnt = 0;
+    for(int i = 0; i < a; i++){
+        int c; cin >> c;
+        if(s.find(c) != s.end()){
+            //cout << "pp" << '\n';
+            continue;
+        }
+        s.insert(c);
+    }
+    set<int>::iterator it;
+    while(cnt != b){
+        cout << *s.rbegin() << ' ';
+        s.erase(*s.rbegin());
+        cnt++;
+    }
+    return 0;
+}
