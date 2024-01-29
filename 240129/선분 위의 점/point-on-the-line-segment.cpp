79be704114f13d2cdf1ec48059536be32a30f769
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-01-29 19:30:52 +0900

    Title: 선분 위의 점, Time: 21ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240129/선분 위의 점/point-on-the-line-segment.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+vector<int> v;
+
+int upper(int x){
+    int left = 0;
+    int right = v.size() - 1;
+    int minidx = n + 2;
+    while(left <= right){
+        int mid = (left + right) / 2;
+        if(v[mid] > x){
+            right = mid - 1;
+            minidx = min(minidx, mid);
+        }
+        else{
+            left = mid + 1;
+        }
+    }
+    return minidx;
+}
+
+int lower(int x){
+    int left = 0;
+    int right = v.size() - 1;
+    int minidx = n + 2;
+    while(left <= right){
+        int mid = (left + right) / 2;
+        if(v[mid] >= x){
+            right = mid - 1;
+            minidx = min(minidx, mid);
+        }
+        else{
+            left = mid + 1;
+        }
+    }
+    return minidx;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> m;
+    v.push_back(0);
+    for(int i = 0; i < n; i++){
+        int a; cin >> a;
+        v.push_back(a);
+    }
+    v.push_back(1000000001);
+    for(int i = 0; i < m; i++){
+        int x1, x2; cin >> x1 >> x2;
+        int ans1 = upper(x2);
+        int ans2 = lower(x1);
+        //cout << ans1 << ' ' << ans2 << '\n';
+        cout << ans1 - ans2 << '\n';
+    }
+    return 0;
+}
