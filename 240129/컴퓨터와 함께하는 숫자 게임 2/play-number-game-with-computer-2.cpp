5c7572b8e3188c470c25b7edd5d28ac83712b5fe
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-01-29 20:00:40 +0900

    Title: 컴퓨터와 함께하는 숫자 게임 2, Time: 300ms, Memory: 80MB, Status: Memory Limit Exceed - CodeTree

---
+++ 240129/컴퓨터와 함께하는 숫자 게임 2/play-number-game-with-computer-2.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <climits>
+using namespace std;
+
+int a;
+int b, c;
+vector<int> v;
+
+int func(int x){
+    int left = 0;
+    int right = v.size() - 1;
+    int cnt = 0;
+    while(left <= right){
+        int mid = (left + right) / 2;
+        if(v[mid] == x){
+            break;
+        }
+        else if(v[mid] > x){
+            right = mid - 1;
+            cnt++;
+        }
+        else{
+            left = mid + 1;
+            cnt++;
+        }
+    }
+    return cnt + 1;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> a;
+    cin >> b >> c;
+    for(int i = 1; i <= a; i++){
+        v.push_back(i);
+    }
+    int minans = INT_MAX;
+    int maxans = 0;
+    for(int i = b; i <= c; i++){
+        int ans = func(i);
+        //cout << ans << ' ';
+        minans = min(minans, ans);
+        maxans = max(maxans, ans);
+    }
+    //cout << '\n';
+    cout << minans << ' ' << maxans;
+    return 0;
+}
