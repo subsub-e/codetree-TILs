a9a38b133d8d97cd1c3ba84fb67e9c37c91d3b66
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-01-29 15:50:06 +0900

    Title: 컴퓨터 이용시간, Time: 96ms, Memory: 5MB, Status: Passed - CodeTree

---
+++ 240129/컴퓨터 이용시간/computer-hours.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <tuple>
+#include <algorithm>
+#include <vector>
+#include <queue>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a; cin >> a;
+    vector<pair<int, int>> aaa;
+    priority_queue<int> computer;
+    int assigned_nums[a];
+    for(int i = 0; i < a; i++){
+        int x1, x2;
+        cin >> x1 >> x2;
+        aaa.push_back(make_pair(x1, x2));
+    }
+    
+    for(int i = 1; i <= a; i++){
+        computer.push(-i);
+    }
+    
+    vector<tuple<int, int, int>> points;
+    for(int i = 0; i < a; i++){
+        int x1, x2;
+        tie(x1, x2) = aaa[i];
+        points.push_back(make_tuple(x1, +1, i));
+        points.push_back(make_tuple(x2, -1, i));
+    }
+
+    sort(points.begin(), points.end());
+    for(int i= 0; i < 2 * a; i++){
+        int x, v, index;
+        tie(x, v, index) = points[i];
+
+        if(v == +1){
+            int computer_num = computer.top();
+            computer.pop();
+
+            assigned_nums[index] = -computer_num;
+        }
+        else{
+            int computer_num = assigned_nums[index];
+            computer.push(-computer_num);
+        }
+    }
+
+    for(int i = 0; i < a; i++){
+        cout << assigned_nums[i] << ' ';
+    }
+    return 0;
+}
