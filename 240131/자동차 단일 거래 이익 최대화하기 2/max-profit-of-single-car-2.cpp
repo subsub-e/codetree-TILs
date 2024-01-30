850cb28651388b4a9b3df0cc633ceffaebed4b73
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-01-31 01:13:29 +0900

    Title: 자동차 단일 거래 이익 최대화하기 2, Time: 50ms, Memory: 1MB, Status: Wrong Answer - CodeTree

--- 240131/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.cpp
+++ 240131/자동차 단일 거래 이익 최대화하기 2/max-profit-of-single-car-2.cpp
@@ -1,27 +1,34 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
+#include <climits>
 using namespace std;
 
 int main() {
     // 여기에 코드를 작성해주세요.
     int a; cin >> a;
-    vector<int> v;
+    vector<long long> v;
     for(int i = 0; i < a; i++){
-        int x; cin >> x;
+        long long x; cin >> x;
         v.push_back(x);
     }
     long long maxans = 0;
+    long long minans = LONG_LONG_MAX;
+    //cout << minans;
     long long sum = 0;
     for(int i = 1; i < a; i++){
         if(v[i] - v[i-1] < 0){
-            maxans = max(maxans, sum);
+            //maxans = max(maxans, sum);
+            minans = LONG_LONG_MAX;
+            maxans = 0;
             sum = 0;
             continue;
         }
-        sum += (v[i] - v[i - 1]);
-        maxans = max(maxans, sum);
+        //sum += (v[i] - v[i - 1]);
+        maxans = max(maxans, v[i]);
+        minans = min(minans, v[i-1]);
+        sum = max(sum, maxans - minans);
     }
-    cout << maxans;
+    cout << sum;
     return 0;
 }
