cd353bdfbd8b898011f7d0ec3c49f1d60698adaa
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-01-30 23:27:13 +0900

    Title: 최대 숫자 만들기, Time: 11ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240130/최대 숫자 만들기/make-biggest-num.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+
+bool cmp(string a, string b){
+    string str1 = a + b;
+    string str2 = b + a;
+    int num1 = stoi(str1);
+    int num2 = stoi(str2);
+    if(num1 > num2){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a; cin >> a;
+    vector<string> v;
+    for(int i = 0; i < a; i++){
+        string str;
+        cin >> str;
+        v.push_back(str);
+    }
+    sort(v.begin(), v.end(), cmp);
+    for(int i = 0; i < a; i++){
+        cout << v[i];
+    }
+    return 0;
+}
