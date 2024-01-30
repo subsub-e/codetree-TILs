5aa7fa748ed2684b83b1de985c50624f6f55fd08
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-01-30 20:54:23 +0900

    Title: 정수 분배하기, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240130/정수 분배하기/distributing-integers.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int n, k;
+vector<int> v;
+
+int func(int x){
+    int cnt = 0;
+    for(int i = 0; i < v.size(); i++){
+        cnt += (v[i]) / x;
+    }
+    //cout << cnt << '\n';
+    return cnt;
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> k;
+    int right = INT_MAX;
+    int left = 1;
+    int maxans = 0;
+    for(int i = 0; i < n; i++){
+        int a; cin >> a;
+        v.push_back(a);
+        right = min(right, a);
+    }
+
+    while(left <= right){
+        int mid = (left + right) / 2;
+        if(func(mid) >= k){
+            left = mid + 1;
+            maxans = max(maxans, mid);
+        }
+        else{
+            right = mid - 1;
+        }
+    }
+    cout << maxans;
+    return 0;
+}
