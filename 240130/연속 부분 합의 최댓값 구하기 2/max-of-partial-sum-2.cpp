4ffe732f356e7508cbc7e3cc9ce10a561f403443
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-01-30 22:05:44 +0900

    Title: 연속 부분 합의 최댓값 구하기 2, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240130/연속 부분 합의 최댓값 구하기 2/max-of-partial-sum-2.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int a; cin >> a;
+    vector<int> v;
+    int sum = 0;
+    int maxans = 0;
+    for(int i = 0; i < a; i++){
+        int x; cin >> x;
+        v.push_back(x);
+    }
+    for(int i = 0; i < a; i++){
+        sum += v[i];
+        if(sum <= 0){
+            sum = 0;
+        }
+        else{
+            maxans = max(maxans, sum);
+        }
+
+    }
+    cout << maxans;
+    return 0;
+}
