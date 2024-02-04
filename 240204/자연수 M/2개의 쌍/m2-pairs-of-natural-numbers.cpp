4b4d23dd612123cfb433517dfdadcbb7566c8c71
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-02-04 21:21:24 +0900

    Title: 자연수 M/2개의 쌍, Time: 108ms, Memory: 1MB, Status: Passed - CodeTree

--- 240204/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.cpp
+++ 240204/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.cpp
@@ -1,35 +1,63 @@
 #include <iostream>
-#include <vector>
 #include <algorithm>
+#include <vector>
+#include <tuple>
+
 using namespace std;
 
+// 변수 선언
+int n;
+vector<pair<int, int> > nums;
+
+int ans;
+
 int main() {
-    // 여기에 코드를 작성해주세요.
-    int n; cin >> n;
-    vector<pair<int, int> > v;
-    int sum = 0;
-    for(int i = 0; i < n; i++){
-        int x, y; cin >> x >> y;
-        sum += x;
-        v.push_back(make_pair(y, x));
+    // 입력:
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        int x, y;
+        cin >> x >> y;
+        nums.push_back(make_pair(y, x));
     }
-    sum /= 2;
-    sort(v.begin(), v.end());
-    // for(int i = 0; i < n; i++){
-    //     cout << v[i].first << ' ' << v[i].second << '\n';
-    // }
-    // cout << sum;
-    int index = 0;
-    int ans = 0;
-    for(int i = 0; i < n; i++){
-        index += v[i].second;
-        if(index == sum){
-            ans = v[i].first + v[i + 1].first;
-            break;
+
+    // y 기준으로 오름차순 정렬을 진행합니다.
+    sort(nums.begin(), nums.end());
+
+    // 최선의 전략을 오름차순 정렬 뒤
+    // 가장 작은 수와 큰 수를 하나씩 매칭해주는 것입니다.
+    int li = 0, ri = n - 1;
+    while(li <= ri) {
+        int ly, lx;
+        tie(ly, lx) = nums[li];
+        int ry, rx;
+        tie(ry, rx) = nums[ri];
+
+        // 답을 갱신합니다.
+        ans = max(ans, ly + ry);
+
+        // 왼쪽 개수가 더 적다면
+        // 왼쪽을 전부 매칭시켜줍니다.
+        if(lx < rx) {
+            // 오른쪽은 lx만큼 개수를 줄여줍니다.
+            nums[ri] = make_pair(ry, rx - lx);
+
+            // 왼쪽 위치를 한칸 뒤로 옮겨줍니다.
+            li++;
         }
-        else if(index > sum){
-            ans = v[i].first * 2;
-            break;
+        // 오른쪽 개수가 더 적다면
+        // 오른쪽을 전부 매칭시켜줍니다.
+        else if(lx > rx) {
+            // 왼쪽은 rx만큼 개수를 줄여줍니다.
+            nums[li] = make_pair(ly, lx - rx);
+
+            // 오른쪽 위치를 한칸 앞으로 옮겨줍니다.
+            ri--;
+        }
+        // 개수가 동일하다면
+        // li, ri 위치 모두 옮겨줍니다.
+        else {
+            li++;
+            ri--;
         }
     }
     cout << ans;
