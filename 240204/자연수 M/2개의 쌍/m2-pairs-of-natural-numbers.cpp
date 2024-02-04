01c3efa586bd91df57062420513fc68aacf58411
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-02-04 21:19:34 +0900

    Title: 자연수 M/2개의 쌍, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240204/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.cpp
+++ 240204/자연수 M/2개의 쌍/m2-pairs-of-natural-numbers.cpp
@@ -6,19 +6,32 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int n; cin >> n;
-    vector<int> v;
+    vector<pair<int, int> > v;
+    int sum = 0;
     for(int i = 0; i < n; i++){
-        int a, b; cin >> a >> b;
-        for(int i = 0; i < a; i++){
-            v.push_back(b);
+        int x, y; cin >> x >> y;
+        sum += x;
+        v.push_back(make_pair(y, x));
+    }
+    sum /= 2;
+    sort(v.begin(), v.end());
+    // for(int i = 0; i < n; i++){
+    //     cout << v[i].first << ' ' << v[i].second << '\n';
+    // }
+    // cout << sum;
+    int index = 0;
+    int ans = 0;
+    for(int i = 0; i < n; i++){
+        index += v[i].second;
+        if(index == sum){
+            ans = v[i].first + v[i + 1].first;
+            break;
+        }
+        else if(index > sum){
+            ans = v[i].first * 2;
+            break;
         }
     }
-    sort(v.begin(), v.end());
-    int maxans = 0;
-    for(int i = 0; i < v.size() / 2; i++){
-        int ans = v[i] + v[v.size() - i - 1];
-        maxans = max(maxans, ans);
-    }
-    cout << maxans;
+    cout << ans;
     return 0;
 }
