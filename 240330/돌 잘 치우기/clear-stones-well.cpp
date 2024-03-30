3d2b9d31c0bda57a50310ef1969deca83ac39754
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-03-30 19:42:22 +0900

    Title: 돌 잘 치우기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240330/돌 잘 치우기/clear-stones-well.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <cstring>
+using namespace std;
+
+int n, m, k;
+int arr[101][101];
+bool visited[101][101];
+int maxans;
+queue<pair<int, int> > q;
+vector<pair<int, int> > stone;
+vector<int> v;
+int dx[4] = {-1, 0, 1, 0};
+int dy[4] = {0, -1, 0, 1};
+
+int bfs(){
+    int cnt = 0;
+    while(!q.empty()){
+        int x = q.front().first;
+        int y = q.front().second;
+        q.pop();
+
+        for(int i = 0; i < 4; i++){
+            int nx = x + dx[i];
+            int ny = y + dy[i];
+            if(0 <= nx && nx < n && 0 <= ny && ny < n && arr[nx][ny] == 0 && !visited[nx][ny]){
+                cnt++;
+                visited[nx][ny] = 1;
+                q.push(make_pair(nx, ny));
+            }
+        }
+    }
+    return cnt;
+}
+
+void func(int x, int y, int cnt){
+    if(v.size() == k){
+        memset(visited, 0, sizeof(visited));
+        for(int i = 0; i < k; i++){
+            int stone_x = stone[v[i]].first;
+            int stone_y = stone[v[i]].second;
+            arr[stone_x][stone_y] = 0;
+        }
+
+        q.push(make_pair(x, y));
+        visited[x][y] = 1;
+        maxans = max(maxans, bfs());
+
+        for(int i = 0; i < k; i++){
+            int stone_x = stone[v[i]].first;
+            int stone_y = stone[v[i]].second;
+            arr[stone_x][stone_y] = 1;
+        }
+        return;
+    }
+    
+
+    for(int i = cnt; i < stone.size(); i++){
+        v.push_back(i);
+        func(x, y, i + 1);
+        v.pop_back();
+    }
+}
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    cin >> n >> m >> k;
+    for(int i = 0; i < n; i++){
+        for(int j = 0; j < n; j++){
+            cin >> arr[i][j];
+            if(arr[i][j] == 1){
+                stone.push_back(make_pair(i, j));
+            }
+        }
+    }
+
+    for(int i = 0; i < m; i++){
+        int x1, x2;
+        cin >> x1 >> x2;
+        func(x1, x2, 0);
+    }
+    cout << maxans;
+    return 0;
+}
