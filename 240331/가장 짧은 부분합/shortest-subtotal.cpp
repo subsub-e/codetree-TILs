fe60fe6f97a70818213c2b51c10c0062e9d8cacd
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-03-31 00:53:45 +0900

    Title: 가장 짧은 부분합, Time: 29ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240331/가장 짧은 부분합/shortest-subtotal.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n;
+    long long m;
+    vector<int> v;
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        int x; cin >> x;
+        v.push_back(x);
+    }
+
+    long long sum = 0;
+    int en = 0;
+    int minans = INT_MAX;
+    for(int st = 0; st < n; st++){
+        while(sum + v[en] < m && en < n){
+            sum += v[en];
+            en++;
+        }
+
+        minans = min(minans, en - st + 1);
+
+        sum -= v[st];
+    }
+    cout << minans;
+    return 0;
+}
