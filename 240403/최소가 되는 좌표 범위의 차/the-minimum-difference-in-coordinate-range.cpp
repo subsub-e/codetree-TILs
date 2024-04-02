9797b3f02a134e5a760bf00dec3df3155a364d3b
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-04-03 01:17:08 +0900

    Title: 최소가 되는 좌표 범위의 차, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240403/최소가 되는 좌표 범위의 차/the-minimum-difference-in-coordinate-range.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+int main() {
+    // 여기에 코드를 작성해주세요.
+    int n, m; cin >> n >> m;
+    vector<pair<int, int> > v;
+    for(int i = 0; i < n; i++){
+        int x1, x2; cin >> x1 >> x2;
+        v.push_back(make_pair(x2, x1));
+    }
+    sort(v.begin(), v.end());
+
+    int minans = INT_MAX;
+    int st = 0;
+    int en = 1;
+    while(st < n - 1){
+        while(en < n && abs(v[en].first - v[st].first) >= m){
+            int temp = (v[en].second - v[st].second);
+            minans = min(minans, abs(temp));
+            en++;
+        }
+        if(en == n){
+            for(int i = st; i < n; i++){
+                int temp = (v[en - 1].second - v[st].second);
+                minans = min(minans, abs(temp));
+            }
+            break;
+        }
+        int temp = (v[en].second - v[st].second);
+        minans = min(minans, abs(temp));
+        if(st + 1 == en){
+            en++;
+        }
+        else{
+            st++;
+        }
+    }
+    if(minans == INT_MAX){
+        cout << -1;
+    }
+    else{
+        cout << minans;
+    }
+
+    return 0;
+}
