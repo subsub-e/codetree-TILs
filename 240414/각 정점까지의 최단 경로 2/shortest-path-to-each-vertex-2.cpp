ccfe8746ae8b641211132bb4d74ce5560d80d919
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-04-14 23:06:25 +0900

    Title: 각 정점까지의 최단 경로 2, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240414/각 정점까지의 최단 경로 2/shortest-path-to-each-vertex-2.cpp
+++ 240414/각 정점까지의 최단 경로 2/shortest-path-to-each-vertex-2.cpp
@@ -35,6 +35,10 @@ int main() {
 
     for(int i = 1; i <= n; i++){
         for(int j = 1; j <= n; j++){
+            if(arr[i][j] == INF{
+                cout << -1 << ' ';
+                continue;
+            }
             cout << arr[i][j] << ' ';
         }
         cout << '\n';
