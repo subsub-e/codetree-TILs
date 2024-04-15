94d68d91e12906b92b978c2462644c1d159cdfb7
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-04-15 21:06:19 +0900

    Title: 크기 비교, Time: 9ms, Memory: 0MB, Status: Passed - CodeTree

--- 240415/크기 비교/size-comparison.cpp
+++ 240415/크기 비교/size-comparison.cpp
@@ -10,16 +10,6 @@ const int INF= 0x3f3f3f3f;
 int main() {
     // 여기에 코드를 작성해주세요.
     cin >> n >> m;
-    if(m == 0){
-        for(int i = 1; i <= n; i++){
-            cout << n-1 << '\n';
-        }
-        return 0;
-    }
-    for(int i = 1; i <= n; i++){
-        fill(arr[i], arr[i] + 1 + n, INF);
-        arr[i][i] = 1;
-    }
 
     while(m--){
         int x1, x2; cin >> x1 >> x2;
@@ -29,9 +19,8 @@ int main() {
     for(int k = 1; k <= n; k++){
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= n; j++){
-                if(arr[i][j] >= arr[i][k] + arr[k][j]){
-                    arr[i][j] = min(arr[i][j], arr[i][k] + arr[k][j]);
-                    arr[k][i] = 1;
+                if(arr[i][k] && arr[k][j]){
+                    arr[i][j] = 1;
                 }
             }
         }
@@ -40,17 +29,12 @@ int main() {
     for(int i = 1; i <= n; i++){
         int cnt = 0;
         for(int j = 1; j <= n; j++){
-            if(arr[i][j] >= INF){
-                cnt++;
+            if(i == j || arr[i][j] || arr[j][i]){
+                continue;
             }
+            cnt++;
         }
-        if(cnt == n-1){
-            cout << 0 << '\n';
-        }
-        else{
-            cout << cnt << '\n';
-        }
-        
+        cout << cnt << '\n';
     }
     return 0;
 }
