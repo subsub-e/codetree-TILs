0489609a1fc562d1233f03396590999893ce5976
subsub-e <55149395+subsub-e@users.noreply.github.com>
2024-04-17 17:15:38 +0900

    Title: 트리 순회, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

--- 240417/트리 순회/the-tree-traversal.cpp
+++ 240417/트리 순회/the-tree-traversal.cpp
@@ -4,57 +4,55 @@
 #include <unordered_map>
 using namespace std;
 
-unordered_map<char, int> m;
-unordered_map<int, char> m1;
+unordered_map<char, pair<char, char> > m;
 int n;
 
-void preorder(int x){
-    if(m1[x] == '.'){
-        return;
+void preorder(char x){
+    cout << x;
+    if(m[x].first != '.'){
+        preorder(m[x].first);
     }
-    cout << m1[x];
-    preorder(x * 2);
-    preorder(x * 2 + 1);
+    if(m[x].second != '.'){
+        preorder(m[x].second);
+    }
 }
 
-void inorder(int x){
-    if(m1[x] == '.'){
-        return;
+void inorder(char x){
+    
+    if(m[x].first != '.'){
+        inorder(m[x].first);
     }
-    inorder(x * 2);
-    cout << m1[x];
-    inorder(x * 2 + 1);
+    cout << x;
+    if(m[x].second != '.'){
+        inorder(m[x].second);
+    }
 }
 
-void postorder(int x){
-    if(m1[x] == '.'){
-        return;
+void postorder(char x){
+    
+    if(m[x].first != '.'){
+        postorder(m[x].first);
     }
-    postorder(x * 2);
-    postorder(x * 2 + 1);
-    cout << m1[x];
+    if(m[x].second != '.'){
+        postorder(m[x].second);
+    }
+    cout << x;
 }
 
 int main() {
     // 여기에 코드를 작성해주세요.
     cin >> n;
-    m['A'] = 1;
-    m1[1] = 'A';
     for(int i = 0; i < n; i++){
         char a, b, c;
         cin >> a >> b >> c;
-        int index = m[a];
-        m[b] = index * 2;
-        m1[index * 2] = b;
-        m[c] = index * 2 + 1;
-        m1[index * 2 + 1] = c;  
+        m[a] = make_pair(b, c); 
     }
 
-    preorder(1);
+    preorder('A');
     cout << '\n';
-    inorder(1);
+    inorder('A');
     cout << '\n';
-    postorder(1);
+    postorder('A');
 
 
     return 0;
